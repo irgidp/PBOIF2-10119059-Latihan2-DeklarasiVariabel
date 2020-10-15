de599f757f72de63b00f9382c11a8aee0500baca
irgidp <irgijak@gmail.com>
2020-10-15 21:54:24 +0700

    Latihan 2

---
+++ PBOIF210119059Latihan2DeklarasiVariabel.java
@@ -0,0 +1,41 @@
+/*
+ * @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan deklarasi variable
+ */
+package pboif2.pkg10119059.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author Corazon
+ */
+public class PBOIF210119059Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+    // TODO code application logic here
+    
+    //Deklarasi Variable
+    int nilaiInt;
+    final double PHI = 3.14; //Kons uppercase
+    boolean nilaiLogika;
+    char nilaiKarakter;
+    
+    //Memberi Nilai ke Variable
+    nilaiInt = 78;
+    nilaiLogika = false;
+    nilaiKarakter = 'D';
+    
+    //Menampilkan Hasil
+    System.out.println("");
+    System.out.println("Isi Variable nilai = " + nilaiInt);
+    System.out.println("Isi Variable PHI = " + PHI);
+    System.out.println("Isi Variable logika = " + nilaiLogika);
+    System.out.println("Isi Variable karakter = " + nilaiKarakter);
+    }
+    
+}
